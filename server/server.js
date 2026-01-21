239c56ad65b18c268f4fc1738a4c710e992eb2e0
Sudarshan Patil <sudarshanpatil486@gmail.com>
2026-01-21 14:38:57 +0530

    Fix CORS preflight for Vercel

--- server/server.js
+++ server/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "http://localhost:5173","https://www.aparaitech.org/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+app.options(/.*/, cors());
 
 app.use(express.json());
 
@@ -21,5 +27,9 @@ app.use("/api/subscribe", subscriberUser);
 app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
+
+// app.listen(5000, ()=> {
+//   console.log('Listening');
+// })
 
 export default app;
