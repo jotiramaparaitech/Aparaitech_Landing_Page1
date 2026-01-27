406ed89c4d663313635b22cd8859985a6ec2838d
jotiramaparaitech <jotiram.aparaitech@gmail.com>
2026-01-27 12:45:44 +0530

    Remove unused routes from ContactRoutes

--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { createContact, createSupportTicket, getContacts } from "../controllers/contactController.js";
+import { createContact} from "../controllers/contactController.js";
 
 const router = express.Router();
 
-router.get("/", getContacts);
 router.post("/", createContact);
-router.post("/support", createSupportTicket);
 
 export default router;
